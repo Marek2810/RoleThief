8a80e1bdcb91f99fa447cd26c3298e3c25e2746e
Marek2810 <marek281010@gmail.com>
2022-11-09 12:19:20 +0100

    Removing duplicate code

--- src/me/Marek2810/RoleThief/Listeners/ThiefInventoryEvent.java
+++ src/me/Marek2810/RoleThief/Listeners/ThiefInventoryEvent.java
@@ -1,8 +1,8 @@
 package me.Marek2810.RoleThief.Listeners;
 
 import java.util.HashMap;
-import java.util.Objects;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -10,11 +10,12 @@ import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import me.Marek2810.RoleThief.Main;
 import me.Marek2810.RoleThief.Commands.Thief;
+import me.Marek2810.RoleThief.Utils.InventoryUtils;
 import net.md_5.bungee.api.ChatColor;
 
 public class ThiefInventoryEvent implements Listener {
@@ -35,50 +36,51 @@ public class ThiefInventoryEvent implements Listener {
         
         cd.put(player, System.currentTimeMillis()+250);
         
-        for(int i = 0; i < 36; i++) {
-            if(!(Objects.equals(event.getInventory().getItem(i), Thief.thiefedPlayers.get(player).getInventory().getItem(i)))){
-                event.setCancelled(true);
-                event.getInventory().setItem(i, Thief.thiefedPlayers.get(player).getInventory().getItem(i));
-                player.updateInventory();
-                return;
-            }
-        }        
+        if ( !(InventoryUtils.syncCheck(player, event.getInventory(), Thief.thiefedPlayers.get(player).getInventory())) ) {
+        	ItemStack item = new ItemStack(Material.AIR);
+        	for (int i = 0; i < 36; i++) {
+        		if (Thief.thiefedPlayers.get(player).getInventory().getItem(i) != null) {
+        			item = Thief.thiefedPlayers.get(player).getInventory().getItem(i);
+        		}
+        		event.getInventory().setItem(i, item);
+        	}        	
+        	event.setCancelled(true);
+        	return;
+        }    
 
         new BukkitRunnable() {
 			public void run() {
-				updateInventroy(player, event.getInventory());
+				InventoryUtils.update(player, event.getInventory(), Thief.thiefedPlayers.get(player).getInventory());
 				cancel();
 			} 
 		}.runTaskLater(Main.getPlugin(Main.class), 1); 			
     } 
     
-    public void updateInventroy(Player player, Inventory inv) {
-    	  for(int i = 0; i < 36; i++){
-    	        if(!(Objects.equals(inv.getItem(i), Thief.thiefedPlayers.get(player).getInventory().getItem(i)))){
-    	            Thief.thiefedPlayers.get(player).getInventory().setItem(i, inv.getItem(i));
-    	            player.updateInventory();
-    	        }
-    	    }
-    }
-    
-    
-    //not used yet, will be same as onInventoryClick event probably
     @EventHandler
     public void onInventoryDrag (InventoryDragEvent event) {
     	if ( !(Thief.thiefedInvs.containsValue(event.getInventory())) ) return;
     	if ( event.getInventory().getType().equals(InventoryType.PLAYER) ) return; 
     	
     	Player player = (Player) event.getWhoClicked();
-    	player.sendMessage("Inv: " + event.getInventory().getType());
     	player.sendMessage(ChatColor.GOLD + "Drag event");
-    	player.sendMessage( Thief.thiefedPlayers.get(player).getName() );
     	
-    	new BukkitRunnable() {
+    	if ( !(InventoryUtils.syncCheck(player, event.getInventory(), Thief.thiefedPlayers.get(player).getInventory())) ) {
+        	ItemStack item = new ItemStack(Material.AIR);
+        	for (int i = 0; i < 36; i++) {
+        		if (Thief.thiefedPlayers.get(player).getInventory().getItem(i) != null) {
+        			item = Thief.thiefedPlayers.get(player).getInventory().getItem(i);
+        		}
+        		event.getInventory().setItem(i, item);
+        	}        	
+        	event.setCancelled(true);
+        	return;
+        }    
+
+        new BukkitRunnable() {
 			public void run() {
-				updateInventroy(player, event.getInventory());
+				InventoryUtils.update(player, event.getInventory(), Thief.thiefedPlayers.get(player).getInventory());
 				cancel();
 			} 
-		}.runTaskLater(Main.getPlugin(Main.class), 1);
-
+		}.runTaskLater(Main.getPlugin(Main.class), 1); 	 
     }
 }
