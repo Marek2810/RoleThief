17b2e8ab9a6bd666b2dad3e674777b2b1dd0197b
Marek2810 <marek281010@gmail.com>
2022-11-09 12:24:38 +0100

    Removing uncomited file

--- src/me/Marek2810/RoleThief/Main.java
+++ src/me/Marek2810/RoleThief/Main.java
@@ -6,14 +6,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 import me.Marek2810.RoleThief.Commands.Thief;
 import me.Marek2810.RoleThief.Listeners.InventoryEvent;
 import me.Marek2810.RoleThief.Listeners.ThiefInventoryEvent;
-import me.Marek2810.RoleThief.Listeners.ThiefedPlayerEvent;
 
 public class Main extends JavaPlugin {
     
 	public static Main inst;
 	public static InventoryEvent inventoryEvent;
     public static ThiefInventoryEvent thiefInventoryEvent;
-    public static ThiefedPlayerEvent thiefPlayerEvent;
 	public static ConsoleCommandSender console;    
 
     @Override
@@ -23,7 +21,6 @@ public class Main extends JavaPlugin {
         this.getCommand("thief").setExecutor(new Thief());
         inventoryEvent = new InventoryEvent();
         thiefInventoryEvent = new ThiefInventoryEvent();
-        thiefPlayerEvent = new ThiefedPlayerEvent();
         this.getServer().getPluginManager().registerEvents(inventoryEvent, this);
         this.getServer().getPluginManager().registerEvents(thiefInventoryEvent, this);
         //this.getServer().getPluginManager().registerEvents(thiefPlayerEvent, this);
